af1f46c53b8793eb86003ee4795acbec65f5194b
M. Samsioe <michael.samsioe@gmail.com>
2022-06-14 16:33:07 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType)
             }
         });
     }
+
+    runGame("addition");
 });
 
-function runGame(){
+/**
+ * The main game "loop", called whwn the script is first loaded
+ * and after the user´s anbswear has been processed
+ */
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 + 1);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. aborting`;
+    }
 }
 
 function checkAnswear () {
@@ -41,8 +54,10 @@ function incrementWrongAnswear() {
 
 }
 
-function displayAdditionQuestions() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
